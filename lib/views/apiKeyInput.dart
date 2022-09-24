5712ba1799223961b3f1add2d1c37e42aa387406
marouane bouzyt <salman.kech58@gmail.com>
2022-09-24 19:07:29 +0100

    finished the link input

--- lib/views/apiKeyInput.dart
+++ lib/views/apiKeyInput.dart
@@ -1,13 +1,14 @@
-// ignore_for_file: file_names, implementation_imports, unnecessary_import, camel_case_types, avoid_unnecessary_containers, prefer_const_constructors, unused_import, prefer_const_literals_to_create_immutables, avoid_print
+// ignore_for_file: file_names, implementation_imports, unnecessary_import, camel_case_types, avoid_unnecessary_containers, prefer_const_constructors, unused_import, prefer_const_literals_to_create_immutables, avoid_print, use_build_context_synchronously, annotate_overrides, must_call_super
 
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/src/widgets/container.dart';
 import 'package:flutter/src/widgets/framework.dart';
 import 'package:librairiedumaroc/views/home_page.dart';
+import 'package:input_history_text_field/input_history_text_field.dart';
 
-TextEditingController apiController = TextEditingController();
-String apiKey = apiController.text;
+TextEditingController apiKeyController = TextEditingController();
+String? apiKey;
 
 class apiKeyInput extends StatefulWidget {
   const apiKeyInput({super.key});
@@ -17,60 +18,104 @@ class apiKeyInput extends StatefulWidget {
 }
 
 class _apiKeyInputState extends State<apiKeyInput> {
+  FocusNode? focusNode;
+
+  void initState() {
+    focusNode = FocusNode();
+
+    // listen to focus changes
+    focusNode!.addListener(() {});
+  }
+
+  void setFocus() {
+    FocusScope.of(context).requestFocus(focusNode);
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Column(children: [
-        Expanded(
-            child: Center(
-                child: Image.asset(
+        body: Column(
+      children: [
+        SizedBox(height: MediaQuery.of(context).size.height * 0.15),
+        Center(
+            child: Image.asset(
           'lib/assets/black logo-8.png',
           width: MediaQuery.of(context).size.width * 0.8,
-        ))),
-        Expanded(
-            child: Padding(
-          padding: const EdgeInsets.all(8.0),
+        )),
+        Padding(
+          padding: const EdgeInsets.all(20.0),
           child: Row(
-            crossAxisAlignment: CrossAxisAlignment.center,
-            mainAxisAlignment: MainAxisAlignment.center,
             children: [
               Expanded(
-                flex: 7,
-                child: TextField(
-                  style: TextStyle(color: Colors.black),
-                  controller: apiController,
-                  decoration: InputDecoration(
-                      labelText: "Cle API prive",
-                      labelStyle: TextStyle(color: Colors.blue),
-                      prefixIcon: Icon(Icons.key, color: Colors.blue),
-                      hintText: "Cle API prive",
-                      hintStyle: TextStyle(color: Colors.blue),
-                      border: OutlineInputBorder(
-                          borderRadius: BorderRadius.circular(20),
-                          borderSide: BorderSide(color: Colors.white))),
-                ),
-              ),
+                  flex: 8,
+                  child: InputHistoryTextField(
+                      focusNode: focusNode,
+                      textEditingController: apiKeyController,
+                      historyKey: "01",
+                      onChanged: (value) {
+                        setState(() {
+                          apiKey = value;
+                        });
+                      },
+                      historyListItemLayoutBuilder: (controller, value, index) {
+                        return InkWell(
+                          onTap: () {
+                            apiKeyController.text = value.text;
+                            setState(() {
+                              apiKey = value.text;
+                            });
+                          },
+                          child: Row(
+                            children: [
+                              Expanded(
+                                flex: 1,
+                                child: Padding(
+                                  padding: const EdgeInsets.all(5.0),
+                                  child: Text(
+                                    value.textToSingleLine,
+                                    overflow: TextOverflow.ellipsis,
+                                    style: TextStyle(
+                                        fontWeight: FontWeight.bold),
+                                  ),
+                                ),
+                              ),
+                              IconButton(
+                                padding: EdgeInsets.all(0),
+                                icon: Icon(
+                                  Icons.close,
+                                  size: 16,
+                                  color: Theme.of(context).disabledColor,
+                                ),
+                                onPressed: () {
+                                  controller.remove(value);
+                                },
+                              ),
+                            ],
+                          ),
+                        );
+                      })),
               Expanded(
-                  flex: 1,
-                  child: Center(
-                    child: IconButton(
-                        padding: EdgeInsets.all(0),
-                        onPressed: () {
-                          Navigator.pushReplacement(
-                  context,
-                  MaterialPageRoute(builder: (context) => const HomePage()),
-                );
-                        },
-                        icon: Icon(
-                          Icons.arrow_circle_right,
-                          color: Colors.blue,
-                          size: 50,
-                        )),
-                  )),
+                  child: Padding(
+                padding: EdgeInsets.all(8),
+                child: IconButton(
+                    padding: EdgeInsets.all(0),
+                    onPressed: () {
+                      Navigator.pushReplacement(
+                        context,
+                        MaterialPageRoute(
+                            builder: (context) => const HomePage()),
+                      );
+                    },
+                    icon: Icon(
+                      Icons.arrow_circle_right,
+                      color: Colors.blue,
+                      size: 50,
+                    )),
+              ))
             ],
           ),
-        ))
-      ]),
-    );
+        ),
+      ],
+    ));
   }
 }
